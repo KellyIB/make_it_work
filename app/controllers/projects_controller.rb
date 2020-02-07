b022f4fa9cc67ece2afd0e44e3844141fd8e5738
KellyIB <kellyibard@gmail.com>
2020-02-07 11:05:09 -0700

    add funcionality for user story 2, added route and controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,10 @@
 class ProjectsController < ApplicationController
 
   def show
-    # binding.pry
     @project = Project.find(params[:id])
   end
 
     private
-
     def project_params
       params.permit(:name, :material)
     end
